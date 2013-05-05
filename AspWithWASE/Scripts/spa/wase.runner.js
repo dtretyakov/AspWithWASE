b25f550f95f9092dd08f31e8863549751736003e
Dmitry Tretyakov <dtretyakov@gmail.com>
2013-05-05 13:31:53 +0400

    Updated nuget packages.

--- AspWithWASE/Scripts/spa/wase.runner.js
+++ AspWithWASE/Scripts/spa/wase.runner.js
@@ -2,7 +2,7 @@
 requirejs.config({
     paths: {
         jquery: "scripts/jquery-2.0.0",
-        knockout: "scripts/knockout-2.2.0",
+        knockout: "scripts/knockout-2.2.1",
         toastr: "scripts/toastr",
         md5: "scripts/md5",
         moment: "scripts/moment"
